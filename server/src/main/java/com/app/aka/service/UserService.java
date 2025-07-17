935d9adc0969e62e8ee606872bcc92b053560f8c
qorcodusl <qorcodus03@naver.com>
2025-07-17 23:46:48 +0900

    Feat: 비밀번호 암호화

--- server/src/main/java/com/app/aka/service/UserService.java
+++ server/src/main/java/com/app/aka/service/UserService.java
@@ -4,6 +4,7 @@ import com.app.aka.dto.UserInfoDto;
 import com.app.aka.entity.UserEntity;
 import com.app.aka.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public UserInfoDto getUserInfo(Long userId) {
         UserEntity user = userRepository.findById(userId)
@@ -34,7 +36,14 @@ public class UserService {
         UserEntity user = userRepository.findById(userId)
                 .orElseThrow(() -> new RuntimeException("사용자 없음"));
 
-        user.setPassword(newPassword);
+        //기존 비밀번호와 동일한지 확인
+        if (passwordEncoder.matches(newPassword, user.getPassword())) {
+            throw new RuntimeException("기존 비밀번호와 동일합니다.");
+        }
+
+        //암호화 후 저장
+        String encodePassword = passwordEncoder.encode(newPassword);//암호화 추가
+        user.setPassword(encodePassword);
         userRepository.save(user);
     }
 }

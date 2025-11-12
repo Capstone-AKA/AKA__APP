c61cbaba070e11b52834a1f8f6301b4d3ffee7af
qorcodusl <qorcodus03@naver.com>
2025-11-12 16:45:45 +0900

    Chore : 실시간 브로드캐스트 추가

--- server/src/main/java/com/app/aka/controller/CartItemController.java
+++ server/src/main/java/com/app/aka/controller/CartItemController.java
@@ -6,6 +6,7 @@ import com.app.aka.dto.DeviceProductRequestDto;
 import com.app.aka.service.CartItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,14 +15,26 @@ import org.springframework.web.bind.annotation.*;
 public class CartItemController {
 
     private final CartItemService cartItemService;
+    private final SimpMessagingTemplate messagingTemplate;
 
     // Jetson Nano: 상품 추가
     @PostMapping("/items/from-device")
     public ResponseEntity<CartItemDeltaListDto> addItemsFromDevice(
             @RequestBody DeviceProductRequestDto request
     ) {
-        return ResponseEntity.ok(cartItemService.addItemsFromDevice(request));
+        CartItemDeltaListDto updatedCart = cartItemService.addItemsFromDevice(request);
+
+        // cartNumber 기반으로 실시간 브로드캐스트
+        if (request.getCartNumber() != null) {
+            String topic = "/topic/cart/" + request.getCartNumber();
+            messagingTemplate.convertAndSend(topic, updatedCart);
+            System.out.println("📡 WebSocket 브로드캐스트 완료 → " + topic);
+        }
+
+        // 기존과 동일하게 결과를 HTTP 응답으로 반환
+        return ResponseEntity.ok(updatedCart);
     }
+
 
     // 수량 증가 (+ 버튼)
     @PatchMapping("/items/{cartItemId}/increase")

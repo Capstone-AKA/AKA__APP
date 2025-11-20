66cb7a45090833dc141b5a57ad6e4ffbf988d969
qorcodusl <qorcodus03@naver.com>
2025-11-20 19:24:58 +0900

    Fix: 중복 브로드캐스트 방지

--- server/src/main/java/com/app/aka/controller/CartItemController.java
+++ server/src/main/java/com/app/aka/controller/CartItemController.java
@@ -24,14 +24,14 @@ public class CartItemController {
     ) {
         CartItemDeltaListDto updatedCart = cartItemService.addItemsFromDevice(request);
 
-        // cartNumber 기반으로 실시간 브로드캐스트
+        /* cartNumber 기반으로 실시간 브로드캐스트
         if (request.getCartNumber() != null) {
             String topic = "/topic/cart/" + request.getCartNumber();
             messagingTemplate.convertAndSend(topic, updatedCart);
             System.out.println(" WebSocket 브로드캐스트 완료 → " + topic);
         } else {
             System.out.println("cartNumber가 null이어서 WebSocket 전송 생략됨");
-        }
+        }*/
 
         // 기존과 동일하게 결과를 HTTP 응답으로 반환
         return ResponseEntity.ok(updatedCart);
